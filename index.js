664338b6834d376537337d90cfeecf8f0f05d509
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-10-04 16:37:19 +0100

    Extra Homework Ingredients

---
+++ index.js
@@ -0,0 +1,20 @@
+// NEW RECIPE: SPAGHETTI ALLA CARBONARA RECIPE
+
+// INPUT:
+let spaghetti = 400
+let guanciale = 250
+let eggYolks = 6
+let pecorinoRomano = 50
+let blackPepper = 4
+let slicedGuanciale = 1
+
+// OUTPUT:
+console.log("spaghetti 400g", spaghetti)
+console.log("guanciale 250g", guanciale)
+console.log("eggYolks 6", eggYolks)
+console.log("pecorinoRomano 50g", pecorinoRomano)
+console.log("blackPepper 4g", blackPepper)
+console.log("slicedGuanciale 1cm", slicedGuanciale)
+
+console.log("------- let Cooking begin-------")
+let firstmix = eggYolks + pecorinoRomano
