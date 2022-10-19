b1a0de36a25da7b9f7e48d98b6ef12f85797ec8f
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-10-19 22:12:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // INPUT:
 let spaghetti = 400
 let guanciale = 250
-let eggYolks = 6
+let eggYolks = 6 //18g
 let pecorinoRomano = 50
 let blackPepper = 4
 let slicedGuanciale = 1
@@ -11,10 +11,11 @@ let slicedGuanciale = 1
 // OUTPUT:
 console.log("spaghetti 400g", spaghetti)
 console.log("guanciale 250g", guanciale)
-console.log("eggYolks 6", eggYolks)
+console.log("eggYolks 18g", eggYolks)
 console.log("pecorinoRomano 50g", pecorinoRomano)
 console.log("blackPepper 4g", blackPepper)
 console.log("slicedGuanciale 1cm", slicedGuanciale)
 
 console.log("------- let Cooking begin-------")
-let firstmix = eggYolks + pecorinoRomano
+let mixedPecorino = eggYolks + pecorinoRomano
+console.log("First mix of Eggyork and pecorinoRomano", mixedPecorino)
